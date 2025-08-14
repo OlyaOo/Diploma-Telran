5449ffa69be1bb10247233726cc07ce3b405ed34
olya <o.v.odarjuk@gmail.com>
2025-08-14 21:23:26 +0300

    +HomePage.module.css

--- frontend/src/features/home/pages/HomePage.jsx
+++ frontend/src/features/home/pages/HomePage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ProductOfDayModal from '../components/ProductOfDayModal.jsx';
+import styles from'./HomePage.module.css';
 
-const HomePage = () => (
-  <section>
-    <h1>Welcome to Garden Products</h1>
+export default function HomePage() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles['hero-text']}>
+          <h1>
+            Amazing Discounts<br />
+            on Garden Products!
+          </h1>
+
+          <button type="button" className={styles['btn-checkout']}>
+            <span className={styles['btn-checkout__text']}>Check out</span>
+          </button>
+        </div>
+      </div>
+    {/* <h1>Welcome to Garden Products</h1> */}
     <ProductOfDayModal />
   </section>
 );
-
-export default HomePage;
+}
