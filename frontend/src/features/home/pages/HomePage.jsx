6aec2d6546f7b73877e1f2c31cdb362b59214af2
lena <olenakorzh3@gmail.com>
2025-08-24 19:54:34 +0200

    Added styles for ProductCard and integration into HomePage

--- frontend/src/features/home/pages/HomePage.jsx
+++ frontend/src/features/home/pages/HomePage.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ProductOfDayModal from '../components/ProductOfDayModal.jsx';
+import CategoriesList from '../components/CategoriesList';
 import DiscountBlock from '../components/DiscountForm/DiscountBlock.jsx';
+import ProductCard from '../../products/components/ProductCard';
+import "@styles/categories.css"
 
 const HomePage = () => (
   <section className="home">
     <div className="home__container">
+      <h1>Welcome to Garten Products</h1>
+      <ProductOfDayModal />
       <DiscountBlock />
+      <CategoriesList />
+      <h2>Random Discounted Products</h2>
+      <div className='discounted-products'>
+        <ProductCard product={{
+          id: 1,
+          title: 'Sample Product',
+          image: 'images/product1.jpg',
+          price: 240,
+          discont_price: 199,
+          image: "/product_img/sample.jpg" 
+        }} />
+    </div>
     </div>
   </section>
 );
